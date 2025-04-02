2ba73a4bd74cc5fdd826dc7d25856d2468aa7090
kendra.jade <kindrai.kendra@gmail.com>
2025-04-01 20:33:08 -0500

    Added weather information per project04 specs

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -2,10 +2,22 @@
 
 // Import the Packages
 import { useEffect, useState } from "react"
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function locationFinderClient() { 
-    const [locationInfo, setLocationInfo] = useState({City: 'N/A'}) // USE STATE USES ARRAY, NOT {} 
-                            // && useState(null) null instead of {} to have an unavailable message status
+    
+    // GETTING LOCATION
+    ///////////////////////////////////////////////////////
+    const [locationInfo, setLocationInfo] = useState(
+        {
+            "City"          : 'unavailable',
+            "RegionName"    : 'unavailable',
+            "CountryName"   : 'unavailable',
+            "Latitude"       : 'unavailable',
+            "Longitude"      : 'unavailable',
+        }
+    ) // USE STATE USES ARRAY, NOT {} 
+
 
     const getLocationInfo = async () => {
         const response = await fetch('https://apip.cc/json');
@@ -14,29 +26,79 @@ export default function locationFinderClient() {
 
         // Set the Data
         setLocationInfo(locationData);
-    }
 
+        // Call get weather data
+        getWeatherInfo(locationData.Latitude, locationData.Longitude);
+    }
 
     useEffect(() => {
         getLocationInfo();
     }, []) // make sure to add this array <-- ensures api is only called once
 
+
+
+
+
+    // GETTING WEATHER DATA
+    ///////////////////////////////////////////////////////
+    const [weatherInfo, setWeatherInfo] = useState(
+        {"product" : 'undefined'}
+    ) // USE STATE USES ARRAY, NOT {} 
+
+    // Creating a state for Loading
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    const getWeatherInfo = async (latitude: string | undefined, longitude: string | undefined) => {
+        
+        // If Location's Long & Lat != 'undefined'
+        if(latitude != undefined && longitude != undefined) {
+
+            // Create the API URL
+            const url = "https://www.7timer.info/bin/astro.php?lon="
+                + longitude
+                + "&lat="
+                + latitude
+                + "&ac=0&unit=metric&output=json&tzshift=0";
+
+            console.log(url);
+
+
+            // Call the API
+            const response = await fetch(url);
+            const weatherData = await response.json();
+            console.log(weatherData);
+
+            // Set the Data
+            setWeatherInfo(weatherData);
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, []) // make sure to add this array <-- ensures api is only called once
+
+
     return (
         <>
-            <div className="card">
-                <div className="card-header">
-                    <h1>Location Finder (Client)</h1>
-                </div>
-                <div className="card-body">
-
-                    {/* If locationInfo ==> */}
-                    {locationInfo 
-                        ? <h2><code>{locationInfo.City}, </code></h2> 
-                        : <h2>Location Data Currently Unavailable.</h2>
-                    }
-                    
-                </div>
-            </div>
+            <h1>Location Finder (Client)</h1>
+            <ul>
+                <li>
+                    Region Data:
+                    <ul>
+                        <li>{locationInfo.City}, {locationInfo.RegionName}</li>
+                        <li>{locationInfo.CountryName}</li>
+                    </ul>
+                </li>
+                <li>
+                    Weather Data:
+                    <ul>
+                        <li>Temperature: {isLoading ? <LoadingSpinner /> : <span>{weatherInfo.dataseries[0].temp2m}° C</span>}</li>
+                    </ul>
+                </li>
+            </ul>
+                              
         </>
     )
 }
--- app/components/LocationFinderServer.tsx
+++ app/components/LocationFinderServer.tsx
@@ -5,23 +5,51 @@
 export default async function locationFinderServer() { // maKE THIS A ASYNC FUNCTION
 
 
-        const response = await fetch('https://apip.cc/json');
-        const locationData = await response.json();
+        // Getting Location Data
+        const locationResponse = await fetch('https://apip.cc/json');
+        const locationData = await locationResponse.json();
         console.log(locationData);
         const locationInfo = locationData;
 
 
+        
+            
+        // Getting Weather Data
+        // Create the API URL
+        const url = "https://www.7timer.info/bin/astro.php?lon="
+            + locationData.Longitude
+            + "&lat="
+            + locationData.Latitude
+            + "&ac=0&unit=metric&output=json&tzshift=0";
+
+        // Call the API
+        const weatherResponse = await fetch(url);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+
+        const weatherInfo = weatherData;
+
+        
 
     return (
         <>
-            <div className="card">
-                <div className="card-header">
-                    <h1>Location Finder (Server)</h1>
-                </div>
-                <div className="card-body">
-                    <h2><code>{locationInfo?.City}, {locationInfo?.RegionCode}</code></h2>
-                </div>
-            </div>
+
+            <h1>Location Finder (Server)</h1>
+            <ul>
+                <li>
+                    Region Data:
+                    <ul>
+                        <li>{locationInfo?.City}, {locationInfo?.RegionName}</li>
+                        <li>{locationInfo?.CountryName}</li>
+                    </ul>
+                </li>
+                <li>
+                    Weather Data:
+                    <ul>
+                        <li>Temperature: {weatherInfo?.dataseries[0].temp2m}° C</li>
+                    </ul>
+                </li>
+            </ul>
         </>
     )
 }
